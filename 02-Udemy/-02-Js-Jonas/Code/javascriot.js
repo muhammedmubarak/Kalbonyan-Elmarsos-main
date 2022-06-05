7b04af31f78ac6454fd0ac13f2124a1909180643
muhammedmubarak <74467830+muhammedmubarak@users.noreply.github.com>
2022-06-05 14:28:44 +0200

    Update javascriot.js

--- 02-Udemy/-02-Js-Jonas/Code/javascriot.js
+++ 02-Udemy/-02-Js-Jonas/Code/javascriot.js
@@ -332,28 +332,28 @@ while (i < muhammed.length) {
 
 
 //////Coding Challenge 5: Solution, Part 2
-var john = {
-        fullName: 'john smith',
-        bills: [124, 48, 268, 180, 42],
-        calcTips: function() {
-            this.tips = [];
-            this.finalValues = [];
-            for (var i = 0; i < this.bills.length; i++) {
-                var percentage;
-                var bill = this.bills[i];
-                if (bill < 50) {
-                    percentage = .2;
-                } else if (bill >= 50 && bill < 200) {
-                    percentage = .15;
-                } else {
-                    percentage = .1;
-                }
-                this.tips[i] = bill * percentage;
-                this.finalValues[i] = bill + bill * percentage;
-            }
-        }
+// var john = {
+//         fullName: 'john smith',
+//         bills: [124, 48, 268, 180, 42],
+//         calcTips: function() {
+//             this.tips = [];
+//             this.finalValues = [];
+//             for (var i = 0; i < this.bills.length; i++) {
+//                 var percentage;
+//                 var bill = this.bills[i];
+//                 if (bill < 50) {
+//                     percentage = .2;
+//                 } else if (bill >= 50 && bill < 200) {
+//                     percentage = .15;
+//                 } else {
+//                     percentage = .1;
+//                 }
+//                 this.tips[i] = bill * percentage;
+//                 this.finalValues[i] = bill + bill * percentage;
+//             }
+//         }
 
-    }
+//     }
     //     //// Coding Challenge 5: Solution, Part 2
     // var mark = {
     //     fullName: 'mark smith',
